ecdba8044cb59c1836de8d8ea3cf3e8f0d68db95
bob <bob.jiang@btcchina.com>
2014-12-05 19:53:00 +0800

    modify index page layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 class HomeController < ApplicationController
   def index
     @excellent_topics = Topic.excellent.recent.fields_for_list.includes(:user).limit(20).to_a
-    @latest_topics = Topic.recent.fields_for_list.includes(:user).limit(10).to_a
+    @latest_topics = Topic.recent.fields_for_list.includes(:user).limit(20).to_a
     fresh_when(etag: [@excellent_topics, @latest_topics, SiteConfig.index_html])
   end
 
