6246fb4e248f138bb7017aa0535003034b50a58a
恒温 <lihua.zlh@alibaba-inc.com>
2015-05-01 09:58:58 +0800

    add topic mapping

--- app/models/notification/node_changed.rb
+++ app/models/notification/node_changed.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class Notification::NodeChanged < Notification::Base
-  belongs_to :topic
+  belongs_to :topic, class_name: "Topic"
   belongs_to :node
 
   delegate :name, to: :node, allow_nil: true, prefix: true
