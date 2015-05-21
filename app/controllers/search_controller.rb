ef4c7d006e373964145e982aa697fefe0018ceae
恒温 <lihua.zlh@alibaba-inc.com>
2015-05-21 14:32:51 +0800

    Revert "revert ... 残念"
    This reverts commit 3c508004238d244a2f9de1b22d25368e6bdfe099.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,22 +3,60 @@ class SearchController < ApplicationController
   def index
     @topics = Topic.search(
         sort: [
-        { updated_at: :desc },
-    ],
+            {updated_at: {order: "desc", ignore_unmapped: true}},
+            {excellent:  {order: "desc", ignore_unmapped: true}}
+        ],
         query: {
-        multi_match: {
-        query: params[:q],
-        fields: %w(title body),
-        fuzziness: 2,
-        prefix_length: 5
-    }
-    },
+            multi_match: {
+                query: params[:q],
+                fields: %w(title body^10),
+                fuzziness: 2,
+                prefix_length: 5,
+                operator: :and
+            }
+        },
+
+        # query: {
+        #     filtered: {
+        #         query: {
+        #             multi_match: {
+        #                 fields: %w(title body^10),
+        #                 query: params[:q],
+        #                 fuzziness: 2,
+        #                 prefix_length: 5,
+        #                 operator: :and
+        #             }
+        #         },
+        #         filter: {
+        #             bool: {
+        #                 must: {
+        #                     query: {
+        #                         multi_match: {
+        #                             fields: %w(title body^10),
+        #                             query: params[:q],
+        #                             fuzziness: 2,
+        #                             prefix_length: 5,
+        #                             operator: :or
+        #                         }
+        #                     }
+        #                 },
+        #                 must_not: {
+        #
+        #                 },
+        #                 should: {
+        #
+        #                 }
+        #             }
+        #         }
+        #     }
+        # },
+
         highlight: {
-        fields: {
-        title: {},
-        body: {}
-    }
-    }
+            fields: {
+                title: {},
+                body: {}
+            }
+        }
     ).paginate(page: params[:page], per_page: 10).records
     @count = @topics.total_entries
   end
