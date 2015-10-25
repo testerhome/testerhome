83684b85b86b6f3b7af384783158a1affb4d8d28
恒温 <lihua.zlh@alibaba-inc.com>
2015-10-25 17:29:23 +0800

    修复like不能点击

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,35 +1,31 @@
-# coding: utf-8
 module LikesHelper
   # 赞功能
   # 参数
   # likeable - Like 的对象
   # :cache - 当为 true 时将不会监测用户是否赞过，直接返回未赞过的状态，以用于 cache 的场景
   def likeable_tag(likeable, opts = {})
-    return "" if likeable.blank?
+    return '' if likeable.blank?
 
     # 没登录，并且也没用用 cache 的时候，直接返回会跳转倒登录的
-    if opts[:cache].blank? && current_user.blank?
-      return link_to(raw("<i class=\"fa fa-heart-o\"></i> <span>赞</span>"), new_user_session_path, class: "")
-    end
+    return unlogin_likeable_tag if opts[:cache].blank? && current_user.blank?
 
-    label = "#{likeable.likes_count} 人赞"
-    label = "赞" if likeable.likes_count == 0
+    label = "#{likeable.likes_count} 个赞"
+    label = '' if likeable.likes_count == 0
 
     if opts[:cache].blank? && likeable.liked_by_user?(current_user)
-      title = "取消赞"
-      state = "followed"
-      icon = content_tag("i", "", class: "fa fa-thumbs-up")
+      title = '取消赞'
+      state = 'followed'
+      icon = content_tag('i', '', class: 'fa fa-thumbs-up')
     else
-      title = "赞"
-      state = ""
-      icon = content_tag("i", "", class: "fa fa-thumbs-up")
+      title = '赞'
+      state = ''
+      icon = content_tag('i', '', class: 'fa fa-thumbs-up')
     end
-
     like_label = raw "#{icon} <span>#{label}</span>"
 
-    link_to(like_label, "#", title: title, 'data-count' => likeable.likes_count,
-          'data-state' => state, 'data-type' => likeable.class,'data-id' => likeable.id,
-          class: "likeable #{state}")
+    link_to(like_label, '#', title: title, 'data-count' => likeable.likes_count,
+                             'data-state' => state, 'data-type' => likeable.class, 'data-id' => likeable.id,
+                             class: "likeable #{state}")
   end
 
   private
