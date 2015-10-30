dedad5573613bd7946dd307e0cdaf51fababd126
恒温 <lihua.zlh@alibaba-inc.com>
2015-10-30 08:18:01 +0800

    cover 必选

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -9,5 +9,8 @@ class Ad
   field :topic_author
   mount_uploader :cover, PhotoUploader
 
+  validates_uniqueness_of :topic_id
+  validates_presence_of :cover, :topic_id
+
   ACCESSABLE_ATTRS = [:topic_id, :topic_title, :topic_author, :cover]
 end
