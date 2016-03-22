1a3e3b504fdb0528b25f491e6dc5f77d77713ae8
恒温 <lihua.zlh@alibaba-inc.com>
2016-03-22 10:40:51 +0800

    add metionable topic

--- app/models/notification/mention.rb
+++ app/models/notification/mention.rb
@@ -8,6 +8,7 @@ class Notification::Mention < Notification::Base
     return {} if self.mentionable.blank?
     {
       title: [self.mentionable.user_login, '提及你：'].join(' '),
+      topic_title: self.topic_title,
       content: self.mentionable_body[0, 30],
       content_path: self.content_path
     }
@@ -26,6 +27,18 @@ class Notification::Mention < Notification::Base
       url_helpers.topic_path(self.mentionable.topic_id)
     else
       ''
+    end
+  end
+
+  def topic_title
+    case self.mentionable_type.downcase
+      when 'topic'
+        Topic.find(self.mentionable_id).title
+      when 'reply'
+        return '' if self.mentionable.blank?
+        Topic.find(self.mentionable.topic_id).title
+      else
+        ''
     end
   end
 end
