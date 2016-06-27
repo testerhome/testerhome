391710250c1c484212c262dabead01f5e24b801e
恒温 <lihua.zlh@alibaba-inc.com>
2016-06-28 03:46:45 +0800

    notifications 分组展示

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,6 +5,7 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.notifications.recent.paginate page: params[:page], per_page: 20
+    @notification_groups = @notifications.group_by { |note| note.created_at.to_date }
     current_user.read_notifications(@notifications)
     set_seo_meta('提醒')
   end
