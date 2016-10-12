7f4c76d76f10c22d0816b2521a5eb4433c9cf507
chenhengjie <chenhengjie123@gmail.com>
2016-10-12 23:39:57 +0800

    [Fix]: 修复问答帖子中节点链接仍为普通帖子节点链接的问题

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -3,8 +3,18 @@ module NotesHelper
     node.topics.count
   end
 
-  def render_node_name(name, id)
-    link_to(name, node_topics_path(id), class: 'node')
+  def render_node_questions_count(node)
+    node.questions.count
+  end
+
+  def render_node_name(name, id, type = 'topic')
+    if type == 'question'
+      link_to(name, node_questions_path(id), class: 'node')
+    elsif type == 'topic'
+      link_to(name, node_topics_path(id), class: 'node')
+    else
+      raise TypeError('Unavailable node type.')
+    end
   end
 
   def note_title_tag(note, opts = {})
